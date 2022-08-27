ee038359ee83da2a87134ae79c0aba770a55ddc0
RafaelVargas29 <79914727+RafaelVargas29@users.noreply.github.com>
2022-08-27 15:55:57 -0300

    edição nos arquivos wlcome e web

--- events/routes/web.php
+++ events/routes/web.php
@@ -14,5 +14,27 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $nome = "Rafael";
+    $idade = '22';
+    $profissao = "progrador";
+    $arr =  [1,2,3,4,5];
+    $nomes = ["Mateus", "Maria", "João", "Saulo"];
+
+    return view('welcome', 
+        [
+            'nome' => $nome,
+            'idade' => $idade,
+            'profissao' => $profissao,
+            'arr' => $arr,
+            'nomes' => $nomes
+        ],
+    );
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/products', function () {
+    return view('products');
 });
