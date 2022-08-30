cf52579bbef23157000cf01b81b7c65fd1fdc6a2
RafaelVargas29 <79914727+RafaelVargas29@users.noreply.github.com>
2022-08-30 17:31:26 -0300

    criação do header da página

--- events/resources/views/layouts/main.blade.php
+++ events/resources/views/layouts/main.blade.php
@@ -18,6 +18,33 @@
         <script src="/js/scripts.js"></script>
     </head>
     <body>
+       <header>
+            <nav class="navbar navbar-expand-lg navbar-light">
+                <div class="collapse navbar-collapse" id="navbar">
+                    <a href="/" class="navbar-brand">
+                        <img src="/img/icone.jpg" alt="">
+                    </a>
+
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a href="/" class="nav-link">Eventos</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="/" class="nav-link">Criar Eventos</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="/" class="nav-link">Entrar</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="/" class="nav-link">Cadastrar</a>
+                        </li>
+                    </ul>
+                    
+                </div>
+            </nav>
+       </header>
+
+
         @yield ('content')
        <footer>
             <p>HDC - Events &copy; 2022</p>
