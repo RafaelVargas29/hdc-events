7b046c3dc9be30c0fadeb86ddd20a96d3ea9af87
RafaelVargas29 <79914727+RafaelVargas29@users.noreply.github.com>
2022-08-30 17:36:10 -0300

    adição da função de buca e exbição por nome de produto

--- events/resources/views/products.blade.php
+++ events/resources/views/products.blade.php
@@ -3,6 +3,10 @@
 @section('title', 'Tela de Produtos')
 
 @section('content')
-<h1>Esta é a tela de Produtos!</h1>
-<a href="/">Voltar para home</a>
+    <h1>Esta é a tela de Produtos!</h1>
+    <a href="/">Voltar para home</a>
+    
+    @if($busca != '')
+        <p>O Usuário está buscando por: {{ $busca }}</p>
+    @endif
 @endsection
