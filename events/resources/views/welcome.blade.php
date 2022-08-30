72ed4802a2f9070aa60a8d9241f631a7f163f8cd
RafaelVargas29 <79914727+RafaelVargas29@users.noreply.github.com>
2022-08-30 17:32:37 -0300

    correção de identação no código

--- events/resources/views/welcome.blade.php
+++ events/resources/views/welcome.blade.php
@@ -3,34 +3,35 @@
 @section('title', 'HDC Events')
 
 @section('content')
-<h1>Algum título</h1>
-<img src="/img/banner.jpg" alt="Banner">
-@if(10 > 5)
-    <p>É true</p>
-@endif
+    <h1>Algum título</h1>
+    <img src="/img/banner.jpg" alt="Banner">
+    
+    @if(10 > 5)
+        <p>É true</p>
+    @endif
 
-<p>{{ $nome }}</p>
-
-@if ($nome ==  'Pedro')
-    <p>O nome é Pedro!</p>
-@else
-    <p>O nome não é Pedro</p>
-@endif
-
-@for($i=0; $i< count($arr); $i++)
-    <p> {{ $arr[$i] }}</p>
-@endfor
-
-@foreach($nomes as $nome)
-    <p>{{ $loop->index }}</p>
     <p>{{ $nome }}</p>
-@endforeach
 
-@php
-    echo "";
-    $name = "Rafael";   
-    echo $name;
-@endphp
+    @if ($nome ==  'Pedro')
+        <p>O nome é Pedro!</p>
+    @else
+        <p>O nome não é Pedro</p>
+    @endif
+
+    @for($i=0; $i< count($arr); $i++)
+        <p> {{ $arr[$i] }}</p>
+    @endfor
+
+    @foreach($nomes as $nome)
+        <p>{{ $loop->index }}</p>
+        <p>{{ $nome }}</p>
+    @endforeach
+
+    @php
+        echo "";
+        $name = "Rafael";   
+        echo $name;
+    @endphp
 
 @endsection
 
