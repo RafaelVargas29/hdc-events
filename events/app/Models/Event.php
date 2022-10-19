4839793c4ad0e06b6f80f18388fba412a0b71a7c
RafaelVargas29 <79914727+RafaelVargas29@users.noreply.github.com>
2022-10-19 12:30:10 -0300

    One to many relationship

--- events/app/Models/Event.php
+++ events/app/Models/Event.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Event extends Model
 {
@@ -13,5 +14,9 @@ class Event extends Model
         'items' => 'array'
     ];
 
-    protected $dates = ['date'];    
+    protected $dates = ['date'];   
+    
+    public function user(){
+        return $this->belongsTo('App\Models\user'); 
+    }
 }
