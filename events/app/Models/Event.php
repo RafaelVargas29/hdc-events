3d183b2f536bf8ab4455b09a710088e891385dfc
RafaelVargas29 <79914727+RafaelVargas29@users.noreply.github.com>
2022-10-31 19:59:31 -0300

    Many-to-many relationship (confirm presence functionality)

--- events/app/Models/Event.php
+++ events/app/Models/Event.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Event extends Model
 {
@@ -21,4 +22,9 @@ class Event extends Model
     public function user(){
         return $this->belongsTo('App\Models\user'); 
     }
+
+    public function users (){
+        return $this->belongsToMany('App\Models\User');
+    }
+    
 }
